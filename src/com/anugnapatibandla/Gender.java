31fa28e461484efc86bb21a20d21e1686bd94261
AnugnaPatibandla <Anugnachowdary11@gmail.com>
2021-02-27 10:03:26 -0500

    added attributes

---
+++ src/com/anugnapatibandla/Gender.java
@@ -0,0 +1,4 @@
+package com.anugnapatibandla;
+
+public enum Gender {
+}
---
+++ src/com/anugnapatibandla/week6.java
@@ -0,0 +1,21 @@
+package com.anugnapatibandla;
+
+import java.util.Date;
+
+public class week6 {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Gender gender; //Enum
+    private String address;
+    private String city;
+    private String pinCode;
+    private String cellPhone;
+    private String email;
+    private String department;
+    private float marks[];
+    private float totalMarks;
+    private float percentage;
+    private String grade; //A+, A
+}
